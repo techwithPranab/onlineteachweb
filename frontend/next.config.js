22f370c463eaf2c069642671b017ea2f87dbe37d
Pranab Paul <pranabpaul@Pranabs-MacBook-Air.local>
2026-01-30 18:40:53 -0800

    added fix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:5000/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000'}/api/:path*`,
       },
     ];
   },
